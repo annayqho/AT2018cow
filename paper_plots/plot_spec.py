b0ca52551efb0845c3cabd489ad031d4b6d7a84a
annayqho <annayqho@gmail.com>
2018-07-13 16:07:24 -0700

    Get rid of old spec code

--- paper_plots/plot_spec.py
+++ paper_plots/plot_spec.py
@@ -10,73 +10,52 @@ import numpy as np
 from astropy.table import Table
 from astropy.cosmology import Planck15
 
-def spec_sequence(days, freq, flux, flux_err, lims):
-    colors = ['pink', 'red', 'orange', 'yellow', 'green', 'blue', 'purple', 'black', 'pink', 'red', 'orange', 'yellow', 'green', 'blue', 'purple', 'black']
-
-    for ii,day in enumerate(days):
-        # plot detections
-        print(day)
-        choose = np.where(np.logical_and(t==day, lims==False))[0]
-        #plt.errorbar(
-        #        freq[choose], flux[choose], yerr=flux_err[choose], 
-        #        color=colors[ii], fmt='.')
-        if ii < 8:
-            plt.plot(
-                    freq[choose], flux[choose], color=colors[ii], 
-                    lw=2.0, ls='--')
-        else:
-            plt.plot(
-                    freq[choose], flux[choose], color=colors[ii], 
-                    lw=2.0, ls='-')
-        plt.show()
-
-
-        # plot non-detections
-        #choose = np.where(np.logical_and(t==day, lims==True))[0]
-        #plt.scatter(
-        #        freq[choose], flux[choose]+1, color=colors[ii], marker='v')
-
-    # formatting
-    #plt.xlim(0.1, 360)
-    #plt.ylim(0.1, 42)
-    plt.ylabel("Flux [mJy]", fontsize=12)
-    plt.xscale('log')
-    plt.yscale('log')
-    # plt.text(
-    #         0.10, 0.90, "$Delta t$ = %s d" %day, 
-    #         horizontalalignment='left', verticalalignment='top',
-    #         transform=axarr[ii].transAxes)
-
-    plt.xlabel("Freq [GHz]", fontsize=12)
-    plt.tight_layout()
-    #plt.show()
-    #plt.savefig("spec_sequence.png")
-
-
 if __name__=="__main__":
     dat = Table.read(
         "../data/radio_lc.dat", delimiter="&", format='ascii.no_header')
+    days = np.array(dat['col1'])
     tel = np.array(dat['col2'])
-    choose = np.array([val in np.array(['SMA', 'ATCA', 'ALMA']) for val in tel])
+    freq = np.array(dat['col3']).astype(float)
+    flux_raw = np.array(dat['col4'])
+    flux = np.zeros(len(flux_raw))
+    flux_err = np.zeros(len(flux_raw))
 
-    tel = tel[choose]
-    days = np.array(dat['col1'][choose])
-    freq = np.array(dat['col3'][choose]).astype(float)
-    flux_raw = np.array(dat['col4'][choose])
-    flux = np.array(
-            [float(val.split("pm")[0][1:]) for val in flux_raw])
-    flux_err = np.array(
-            [float(val.split("pm")[1][0:-1]) for val in flux_raw]) 
+    # Now, go through and apply the appropriate uncertainties
+    for ii,val in enumerate(flux_raw):
+        if tel[ii] == 'ALMA':
+            flux[ii] = val.split("$")[1]
+            if freq[ii] < 200: # bands 3 and 4
+                flux_err[ii] = 0.05*flux[ii]
+            else: # bands 7 and 8
+                flux_err[ii] = 0.10*flux[ii]
+        elif tel[ii] == 'SMA':
+            flux[ii] = float(val.split("pm")[0][1:]) 
+            flux_err_temp = float(val.split("pm")[1][0:-1])
+            # add 10% uncertainty
+            flux_err[ii] = np.sqrt(
+                    (0.10*flux[ii])**2 + flux_err_temp**2)
+        elif tel[ii] == 'ATCA':
+            flux[ii] = float(val.split("pm")[0][1:]) 
+            # add 10% uncertainty
+            flux_err_temp = float(val.split("pm")[1][0:-1])
+            if flux_err_temp < 0:
+                flux_err[ii] = -99
+            else:
+                flux_err[ii] = np.sqrt(
+                        (0.10*flux[ii])**2 + flux_err_temp**2)
 
-    # first, try to show the spectrum on one plot
-    fig = plt.figure(figsize=(8,6))
+    fig, axarr = plt.subplots(2, 1, figsize=(8,8))
+
+    # top panel: evolution of spectra
+    ax = axarr[0]
 
     # plot the spectrum from Day 10 data
     choose = days == 10
     order = np.argsort(freq[choose])
-    plt.errorbar(
-            freq[choose][order], flux[choose][order], 
-            yerr=flux_err[choose][order],
+    det = flux_err[choose][order] > 0
+    ax.errorbar(
+            freq[choose][order][det], flux[choose][order][det], 
+            yerr=flux_err[choose][order][det],
             mfc='white', mec='black', fmt=':', marker='o',
             label="$\Delta t = 10$", c='k')
 
@@ -84,34 +63,79 @@ if __name__=="__main__":
     # ATCA from Day 13, SMA and ALMA from Day 14
     choose = np.logical_or(
             np.logical_and(days == 13, tel == 'ATCA'),
-            np.logical_and(days == 14, tel == 'SMA'))
+            np.logical_and(days == 14, tel != 'ATCA'))
 
     order = np.argsort(freq[choose])
-    plt.errorbar(
-            freq[choose][order], flux[choose][order], 
-            yerr=flux_err[choose][order],
+
+    # for detections
+    det = flux_err[choose][order] > 0
+    ax.errorbar(
+            freq[choose][order][det], flux[choose][order][det], 
+            yerr=flux_err[choose][order][det],
             mfc='white', mec='black', fmt='--', marker='s',
-            label="$\Delta t = 13-14$", c='k')
+            label="$\Delta t = 13, 14$", c='k')
 
     # plot the spectrum from the Day 22 data
-    choose = np.logical_and(days >= 22, days <= 24)
+    choose = np.logical_or(days == 22, days == 24)
 
     order = np.argsort(freq[choose])
-    plt.errorbar(
+    ax.errorbar(
             freq[choose][order], flux[choose][order], 
             yerr=flux_err[choose][order],
             mfc='white', mec='black', fmt='-', marker='v', ms=8, ls='-',
-            label="$\Delta t = 22-24$", c='k')
+            label="$\Delta t = 22, 24$", c='k')
 
-    #spec_sequence(day_bins, freq, flux, flux_err, lims)
+    ax.tick_params(axis='both', labelsize=14)
+    ax.tick_params(axis='both', labelsize=14)
+    ax.set_ylabel("Flux [mJy]", fontsize=16)
+    ax.set_xscale('log')
+    ax.set_yscale('log')
 
-    #spectral_index(t, freq, flux, flux_err, lims)
+    ax.legend()
 
-    plt.xticks(fontsize=14)
-    plt.yticks(fontsize=14)
-    plt.xlabel("Frequency [GHz]", fontsize=16)
-    plt.ylabel("Flux [mJy]", fontsize=16)
+    # plot the ALMA Day 22 and SMA Day 24 data by itself as well
+    choose_alma = np.logical_and(days == 22, tel == 'ALMA')
+    choose_sma = np.logical_and(days == 24, tel == 'SMA')
+    choose = np.logical_or(choose_alma, choose_sma)
+    order = np.argsort(freq[choose])
 
-    plt.legend()
+    ax = axarr[1]
+    ax.errorbar(
+            freq[choose][order], flux[choose][order], 
+            yerr=flux_err[choose][order],
+            mec='black', fmt='.', c='k')
+    # temporary ATCA point
+    ax.errorbar(34, 12, yerr=0.1*12, mec='black', fmt='.', c='k')
+    ax.text(
+            0.9, 0.1, "ATCA Day 19, ALMA Day 22, SMA Day 24", 
+            transform=ax.transAxes, horizontalalignment='right', fontsize=14)
 
-    plt.show()
+    # show a fit
+    x = np.linspace(34, 90)
+    y = 13*(x/34)**2
+    plt.plot(x, y, c='black', lw=1, ls='-')
+
+    # shallower part
+    x = np.linspace(90, 120)
+    y = 96*(x/103)**(1/3)
+    plt.plot(x, y, c='black', lw=1, ls='-')
+
+    # power-law index
+    x = np.linspace(120, 350)
+    alpha = -1.11
+    y = 80*((x/150)**alpha)
+    plt.plot(x, y, c='black', lw=1, ls='-')
+
+    # show critical frequencies
+    plt.axvline(x=90, c='grey', ls='--', alpha=0.8)
+    plt.axvline(x=120, c='grey', ls='--', alpha=0.8)
+
+    ax.tick_params(axis='both', labelsize=14)
+    ax.tick_params(axis='both', labelsize=14)
+    ax.set_xlabel("Frequency [GHz]", fontsize=16)
+    ax.set_ylabel("Flux [mJy]", fontsize=16)
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+
+    #plt.show()
+    plt.savefig("spec.png")
