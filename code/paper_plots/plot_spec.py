3fe801c9a57b9d64244d54abc269b44db6553a23
annayqho <annayqho@gmail.com>
2018-08-13 21:50:35 +0200

    fully updated spectrum code

--- code/paper_plots/plot_spec.py
+++ code/paper_plots/plot_spec.py
@@ -19,7 +19,7 @@ from get_radio import get_data_all, get_spectrum
 from synchrotron_fit import self_abs, fit_self_abs
 
 
-def run_day(ax, day, formatting=True):
+def run_day(ax, day, formatting=True, fit_peak=True, quad=False):
     """ Run this for one day """
     nu,flux = get_spectrum(day)
     ax.scatter(
@@ -31,37 +31,46 @@ def run_day(ax, day, formatting=True):
         ax.set_yscale('log')
         ax.set_ylabel("Flux [mJy]", fontsize=16)
         ax.text(
-                0.4, 0.45, "$F_\\nu \propto \\nu^2$", 
-                transform=ax.transAxes, horizontalalignment='right', fontsize=14)
+                0.3, 0.45, "$F_\\nu \propto \\nu^2$", 
+                transform=ax.transAxes, horizontalalignment='right', 
+                fontsize=14)
         ax.text(
                 0.9, 0.1, "Day %s" %day, 
-                transform=ax.transAxes, horizontalalignment='right', fontsize=14)
+                transform=ax.transAxes, horizontalalignment='right', 
+                fontsize=14)
 
-    b = fit_self_abs(nu[nu<100],flux[nu<100])
-    #         flux_err[choose][order][det][0:2])
-    xfit = np.linspace(5, 200)
-    yfit = 10**(2*np.log10(xfit)+b)
-    ax.plot(xfit, yfit, ls=':', c='k')
+    if fit_peak:
+        b = fit_self_abs(nu[nu<70],flux[nu<70])
+        #         flux_err[choose][order][det][0:2])
+        xfit = np.linspace(5, 200)
+        yfit = 10**(2*np.log10(xfit)+b)
+        ax.plot(xfit, yfit, ls=':', c='k')
 
+        # fit and plot a nu^something line
+        xfit = np.linspace(50, 1000)
+        m,b = np.polyfit(
+                np.log10(nu[nu>180]),
+                np.log10(flux[nu>180]),
+                deg = 1)
+        yfit = 10**(m*np.log10(xfit)+b)
+        ax.plot(xfit, yfit, ls=':', c='k')
+        ax.text(
+                0.8, 0.85, "$F_\\nu \propto \\nu^{%s}$" %np.round(m,1), 
+                transform=ax.transAxes, horizontalalignment='left', fontsize=14)
 
-    # fit and plot a nu^something line
-    # m,b = np.polyfit(
-    #         np.log10(freq[choose][order][det][2:]),
-    #         np.log10(flux[choose][order][det][2:]),
-    #         deg = 1,
-    #         w=1/flux_err[choose][order][det][2:]**2)
-    # print(m)
-    xfit = np.linspace(80, 500)
-    # yfit = 10**(m*np.log10(xfit)+b)
-    # ax.plot(xfit, yfit, ls=':', c='k')
-    # ax.text(
-    #         0.8, 0.85, "$F_\\nu \propto \\nu^{-1.8}$", 
-    #         transform=ax.transAxes, horizontalalignment='left', fontsize=14)
+        # the lines join at (144, 92)
+        #ax.text(
+        #        0, 0.9, "$(\\nu_p, F_p) = (144\,\mathrm{GHz}, 92\,\mathrm{mJy}$)", 
+        #        transform=ax.transAxes, horizontalalignment='left', fontsize=14)
 
-    # the lines join at (144, 92)
-    #ax.text(
-    #        0, 0.9, "$(\\nu_p, F_p) = (144\,\mathrm{GHz}, 92\,\mathrm{mJy}$)", 
-    #        transform=ax.transAxes, horizontalalignment='left', fontsize=14)
+    if quad:
+        # quadratic function to find peak
+        nuc = np.logical_and(nu>87.5, nu<107.5)
+        out = np.polyfit(nu[nuc], flux[nuc], deg=2)
+        xlab = np.linspace(87.5, 107.5)
+        ylab = out[0]*xlab**2 + out[1]*xlab + out[2]
+        ax.plot(xlab, ylab, c='k', ls='--')
+
 
     ax.tick_params(axis='both', labelsize=14)
     ax.tick_params(axis='both', labelsize=14)
@@ -81,7 +90,7 @@ if __name__=="__main__":
 
     # bottom panel: spectrum on Day 22
     ax = axarr[2]
-    run_day(ax, 22)
+    run_day(ax, 22, fit_peak=False)
     ax.set_xlabel("Frequency [GHz]", fontsize=16)
 
     # Zoomed-in window
@@ -89,7 +98,7 @@ if __name__=="__main__":
             ax, 2, 1, loc=1,
             bbox_to_anchor=(0.4,0.8),
             bbox_transform=ax.transAxes)
-    run_day(axins, 22, formatting=False)
+    run_day(axins, 22, formatting=False, fit_peak=False, quad=True)
     axins.set_xlim(88, 106)
     axins.set_ylim(90, 95)
     axins.tick_params(axis='both', labelsize=12)
@@ -97,19 +106,7 @@ if __name__=="__main__":
     axins.xaxis.set_major_locator(MaxNLocator(nbins=2, prune='lower'))
     axins.yaxis.set_major_locator(MaxNLocator(nbins=2, prune='lower'))
 
-    # # Choose the Band 3 measurements
-    # freq_fit = freq[choose][order]
-    # flux_fit = flux[choose][order]
-    # choosefit = np.logical_and(freq_fit < 107.5, freq_fit > 87.5)
-    # axins.scatter(freq_fit[choosefit], flux_fit[choosefit], c='k')
-
-    # # Fit a quadratic function to the data to find the peak
-    # out = np.polyfit(freq_fit[choosefit], flux_fit[choosefit], deg=2)
-    # xlab = np.linspace(87.5, 107.5)
-    # ylab = out[0]*xlab**2 + out[1]*xlab + out[2]
-    # axins.plot(xlab, ylab, c='k', ls='--')
 
 
-
-    plt.show()
-    # plt.savefig("spec.png")
+    #plt.show()
+    plt.savefig("spec.png")
