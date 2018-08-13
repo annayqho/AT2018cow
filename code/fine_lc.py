2e5ab91bf5953f502fb31ac10b12e3cc329e7344
annayqho <annayqho@gmail.com>
2018-08-13 18:28:48 +0200

    plot early lc in small bins

--- code/fine_lc.py
+++ code/fine_lc.py
@@ -1,15 +1,19 @@
-""" Light curves binned into smaller time intervals """
+""" 
+Plot the SMA light curves from the first few days,
+binned into smaller time intervals as provided by Glen Petitpas
+"""
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 def weighted_mean(flux, flux_err):
+    """ Return weighted mean of two flux values, using their uncertainties """
     mean = np.sum(flux/flux_err**2, axis=0) / np.sum(1/flux_err**2, axis=0)
     err_mean = np.sqrt(1 / np.sum(1/flux_err**2, axis=0))
     return mean, err_mean
 
 
-# June 21, Day 5
+# Data Glen sent from June 21 (Day 5)
 raw = ["RxA LSB  11.31 +/-1.94  10.48 +/-1.13  15.44 +/-1.11  15.28 +/-1.27",
 "RxA USB  13.07 +/-2.33  12.41 +/-1.33  14.96 +/-1.31  13.43 +/-1.53",
 "RxB LSB  7.34 +/-2.39  12.72 +/-1.57  14.05 +/-1.35  15.36 +/-1.52",
@@ -19,6 +23,12 @@ bands = []
 flux = []
 flux_err = []
 
+# Ultimately, you want a plot with two curves,
+# each with four points,
+# one curve corresponding to the LSB and
+# one curve corresponding to the USB
+
+# First, populate the arrays.
 for line in raw:
     bands.append(line.split("  ")[0])
     vals = line.split("  ")[1:]
@@ -29,8 +39,18 @@ for line in raw:
    
 bands = np.array(bands)
 sb = np.array([val.split(" ")[1] for val in bands])
-flux = np.array(flux)
-flux_err = np.array(flux_err)
+flux = np.array(flux) # 4x4 array
+flux_err = np.array(flux_err) # 4x4 array
 
+# Now you want to take the mean of the two LSB lines
 choose = sb == 'LSB'
 mean_flux, emean_flux = weighted_mean(flux[choose], flux_err[choose])
+npts = len(mean_flux)
+plt.errorbar(np.arange(npts), mean_flux, yerr=emean_flux, c='k', fmt='o')
+choose = sb == 'USB'
+mean_flux, emean_flux = weighted_mean(flux[choose], flux_err[choose])
+npts = len(mean_flux)
+plt.errorbar(np.arange(npts), mean_flux, yerr=emean_flux, c='k', fmt='s')
+plt.show()
+
+# It's just going up slowly. It's not swinging up and down dramatically.
